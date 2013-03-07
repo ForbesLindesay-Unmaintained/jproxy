05fa691305603918d1bf289f62ea8b10ce3bce52
ForbesLindesay <forbes@lindesay.co.uk>
2013-03-07 07:55:46 +0000

    Add debug statements

--- filters/component.js
+++ filters/component.js
@@ -1,3 +1,4 @@
+var debug = require('debug')('jproxy:component');
 var join = require('path').join;
 var dirname = require('path').dirname;
 var fs = require('fs');
@@ -10,13 +11,21 @@ exports.include = ['*/build/build.js', '*/build/build.css'];
 exports.exclude = '';
 
 function apply(directory, filePath, options, res, next) {
+  debug('%j -> %j using %j', directory, filePath, options);
   var dir = filePath.replace(/\/[^\/]+$/g, '/');
+  debug('dir %j', dir);
   var componentPath = join(directory, dir, options.component || '../component.json');
+  debug('component path %j', componentPath);
   var componentDir = dirname(componentPath);
+  debug('component dir %j', componentDir);
   fs.stat(componentPath, function (err) {
-    if (err) return next(err);
+    if (err) {
+      debug('no component.json');
+      return next(err);
+    }
     fs.stat(join(componentDir, 'components'), function (err) {
       if (err) {
+        debug('installing');
         spawn(install, options.dev !== false ? ['--dev'] : [], {cwd: componentDir})
           .on('exit', function () {
             installed();
@@ -27,6 +36,7 @@ function apply(directory, filePath, options, res, next) {
     });
   });
   function installed() {
+    debug('installed');
     var builder = new Builder(componentDir);
     builder.addLookup(join(componentDir, 'components'));
     if (options.dev !== false && options.srcURLs !== false) builder.addSourceURLs();
