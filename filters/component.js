c4169d242c0567aa9b89866553aeb291dfdca064
ForbesLindesay <forbes@lindesay.co.uk>
2013-03-07 04:25:56 +0000

    Fix component filter

--- filters/component.js
+++ filters/component.js
@@ -11,26 +11,27 @@ exports.exclude = '';
 
 function apply(directory, filePath, options, res, next) {
   var dir = filePath.replace(/\/[^\/]+$/g, '/');
-  console.warn('attempting to build ' + JSON.stringify(dir) + ' in ' + directory);
   var componentPath = join(directory, dir, options.component || '../component.json');
   var componentDir = dirname(componentPath);
   fs.stat(componentPath, function (err) {
     if (err) return next(err);
     spawn(install, options.dev !== false ? ['--dev'] : [], {cwd: componentDir})
       .on('exit', function () {
-        var builder = new Builder(directory);
+        var builder = new Builder(componentDir);
         builder.addLookup(join(componentDir, 'components'));
         if (options.dev !== false && options.srcURLs !== false) builder.addSourceURLs();
         if (options.dev !== false) builder.development();
         builder
           .build(function (err, result) {
             if (err) return next(err);
-            if (filePath === '/build/build.js') {
+            if (/\.js$/.test(filePath)) {
               res.setHeader('Content-Type', 'application/javascript');
               res.send(result.require + '\n' + result.js);
-            } else {
+            } else if (/\.css$/.test(filePath)) {
               res.setHeader('Content-Type', 'text/css');
               res.send(result.css);
+            } else {
+              return next();
             }
           });
       });
