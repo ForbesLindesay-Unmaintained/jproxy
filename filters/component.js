364a1522070c2725fb6cc5b71e5b8a064f068bb9
ForbesLindesay <forbes@lindesay.co.uk>
2013-03-07 07:35:33 +0000

    Don't component install after the first time

--- filters/component.js
+++ filters/component.js
@@ -15,25 +15,34 @@ function apply(directory, filePath, options, res, next) {
   var componentDir = dirname(componentPath);
   fs.stat(componentPath, function (err) {
     if (err) return next(err);
-    spawn(install, options.dev !== false ? ['--dev'] : [], {cwd: componentDir})
-      .on('exit', function () {
-        var builder = new Builder(componentDir);
-        builder.addLookup(join(componentDir, 'components'));
-        if (options.dev !== false && options.srcURLs !== false) builder.addSourceURLs();
-        if (options.dev !== false) builder.development();
-        builder
-          .build(function (err, result) {
-            if (err) return next(err);
-            if (/\.js$/.test(filePath)) {
-              res.setHeader('Content-Type', 'application/javascript');
-              res.send(result.require + '\n' + result.js);
-            } else if (/\.css$/.test(filePath)) {
-              res.setHeader('Content-Type', 'text/css');
-              res.send(result.css);
-            } else {
-              return next();
-            }
+    fs.stat(join(componentDir, 'components'), function (err) {
+      if (err) {
+        spawn(install, options.dev !== false ? ['--dev'] : [], {cwd: componentDir})
+          .on('exit', function () {
+            installed();
           });
+      } else {
+        installed();
+      }
+    });
+  });
+  function installed() {
+    var builder = new Builder(componentDir);
+    builder.addLookup(join(componentDir, 'components'));
+    if (options.dev !== false && options.srcURLs !== false) builder.addSourceURLs();
+    if (options.dev !== false) builder.development();
+    builder
+      .build(function (err, result) {
+        if (err) return next(err);
+        if (/\.js$/.test(filePath)) {
+          res.setHeader('Content-Type', 'application/javascript');
+          res.send(result.require + '\n' + result.js);
+        } else if (/\.css$/.test(filePath)) {
+          res.setHeader('Content-Type', 'text/css');
+          res.send(result.css);
+        } else {
+          return next();
+        }
       });
-  })
+  }
 };
